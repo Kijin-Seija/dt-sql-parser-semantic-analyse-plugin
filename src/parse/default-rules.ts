fa4666273dc85abb4904d3c55895945799811b2b
Kijin-Seija <almon123@qq.com>
2024-06-19 20:12:27 +0800

    0.0.1-alpha.15

--- src/parse/default-rules.ts
+++ src/parse/default-rules.ts
@@ -2,12 +2,23 @@ import { PostgreSqlParser } from 'dt-sql-parser/dist/lib/postgresql/PostgreSqlPa
 
 export const defaultAlias = {
   selectstmt: 'selectStatement',
+  insertstmt: 'insertStatement',
   target_el: 'target_label',
-  table_name: 'tableName'
+  table_name: 'tableName',
+  view_name: 'viewName',
+  column_name: 'columnName',
+  schema_name: 'schemaName'
 }
 
 export const defaultStmts = [
   'selectstmt',
+  'insertstmt',
+  'updatestmt',
+  'deletestmt',
+  'altertablestmt',
+  'alterfunctionstmt',
+  'dropstmt',
+  'createfunctionstmt'
 ]
 
 export const defaultEntities = [
@@ -15,7 +26,12 @@ export const defaultEntities = [
   'colid',
   'attr_name',
   'collabel',
-  'table_name'
+  'table_name',
+  'view_name',
+  'function_with_argtypes',
+  'column_name',
+  'schema_name',
+  'anysconst'
 ]
 
 export const defaultRules: Record<string, number[]> = {
@@ -23,6 +39,67 @@ export const defaultRules: Record<string, number[]> = {
     PostgreSqlParser.RULE_selectstmt,
     PostgreSqlParser.RULE_target_el,
   ],
+  select_from_table: [
+    PostgreSqlParser.RULE_selectstmt,
+    PostgreSqlParser.RULE_from_clause,
+    PostgreSqlParser.RULE_table_name
+  ],
+  insert_target_table: [
+    PostgreSqlParser.RULE_insertstmt,
+    PostgreSqlParser.RULE_insert_target,
+    PostgreSqlParser.RULE_table_name
+  ],
+  update_relation_table: [
+    PostgreSqlParser.RULE_updatestmt,
+    PostgreSqlParser.RULE_relation_expr,
+    PostgreSqlParser.RULE_table_name
+  ],
+  delete_relation_table: [
+    PostgreSqlParser.RULE_deletestmt,
+    PostgreSqlParser.RULE_relation_expr,
+    PostgreSqlParser.RULE_table_name
+  ],
+  alter_table: [
+    PostgreSqlParser.RULE_altertablestmt,
+    PostgreSqlParser.RULE_relation_expr,
+    PostgreSqlParser.RULE_table_name
+  ],
+  alter_view: [
+    PostgreSqlParser.RULE_altertablestmt,
+    PostgreSqlParser.RULE_view_name
+  ],
+  alter_function: [
+    PostgreSqlParser.RULE_alterfunctionstmt,
+    PostgreSqlParser.RULE_alterFunctionTypeClause,
+    PostgreSqlParser.RULE_function_with_argtypes
+  ],
+  alter_table_drop_column: [
+    PostgreSqlParser.RULE_altertablestmt,
+    PostgreSqlParser.RULE_alter_table_cmds,
+    PostgreSqlParser.RULE_column_name
+  ],
+  drop_table: [
+    PostgreSqlParser.RULE_dropstmt,
+    PostgreSqlParser.RULE_table_name_list,
+    PostgreSqlParser.RULE_table_name
+  ],
+  drop_view: [
+    PostgreSqlParser.RULE_dropstmt,
+    PostgreSqlParser.RULE_view_nameList,
+    PostgreSqlParser.RULE_view_name
+  ],
+  drop_schema: [
+    PostgreSqlParser.RULE_dropstmt,
+    PostgreSqlParser.RULE_schema_name_list,
+    PostgreSqlParser.RULE_schema_name
+  ],
+  create_function_sub_content: [
+    PostgreSqlParser.RULE_createfunctionstmt,
+    PostgreSqlParser.RULE_createfunc_opt_list,
+    PostgreSqlParser.RULE_colid,
+    PostgreSqlParser.RULE_anysconst,
+  ],
+  // sub entities
   select_target_colid: [
     PostgreSqlParser.RULE_target_el,
     PostgreSqlParser.RULE_function_name,
@@ -38,17 +115,36 @@ export const defaultRules: Record<string, number[]> = {
     -PostgreSqlParser.RULE_attr_name,
     PostgreSqlParser.RULE_collabel
   ],
-  select_from_table: [
-    PostgreSqlParser.RULE_selectstmt,
-    PostgreSqlParser.RULE_from_clause,
-    PostgreSqlParser.RULE_table_name
-  ],
   table_name_colid: [
     PostgreSqlParser.RULE_table_name,
     PostgreSqlParser.RULE_colid
   ],
   table_name_attr: [
     PostgreSqlParser.RULE_table_name,
+    PostgreSqlParser.RULE_attr_name,
+  ],
+  view_name_colid: [
+    PostgreSqlParser.RULE_view_name,
+    PostgreSqlParser.RULE_colid
+  ],
+  view_name_attr: [
+    PostgreSqlParser.RULE_view_name,
+    PostgreSqlParser.RULE_attr_name,
+  ],
+  function_colid: [
+    PostgreSqlParser.RULE_function_with_argtypes,
+    PostgreSqlParser.RULE_colid
+  ],
+  function_attr: [
+    PostgreSqlParser.RULE_function_with_argtypes,
+    PostgreSqlParser.RULE_attr_name,
+  ],
+  column_name_colid: [
+    PostgreSqlParser.RULE_column_name,
+    PostgreSqlParser.RULE_colid
+  ],
+  column_name_attr: [
+    PostgreSqlParser.RULE_column_name,
     PostgreSqlParser.RULE_attr_name,
   ]
 }
